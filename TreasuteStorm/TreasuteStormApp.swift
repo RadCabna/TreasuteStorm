dc32431422643d4b8c222c0eadf39fcd3ad64bc7
Radcabna <radcabna@gmail.com>
2026-01-29 16:31:39 +0300

    Initial Commit

---
+++ TreasuteStorm/TreasuteStormApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasuteStormApp.swift
+//  TreasuteStorm
+//
+//  Created by Алкександр Степанов on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TreasuteStormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
